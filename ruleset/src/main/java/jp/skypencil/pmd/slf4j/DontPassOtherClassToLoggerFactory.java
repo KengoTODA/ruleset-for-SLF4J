c3d657b48fdc833cc09c1374448f2d586148225c
eller86 <skypencil+github@gmail.com>
2012-03-18 19:43:37 +0800

    fixing bugs for DontPassOtherClassToLoggerFactory rule

--- ruleset/src/main/java/jp/skypencil/pmd/slf4j/DontPassOtherClassToLoggerFactory.java
+++ ruleset/src/main/java/jp/skypencil/pmd/slf4j/DontPassOtherClassToLoggerFactory.java
@@ -1,21 +1,39 @@
 package jp.skypencil.pmd.slf4j;
 
 import java.util.Deque;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 import net.sourceforge.pmd.AbstractJavaRule;
 import net.sourceforge.pmd.ast.ASTClassOrInterfaceDeclaration;
 import net.sourceforge.pmd.ast.ASTClassOrInterfaceType;
 import net.sourceforge.pmd.ast.ASTFieldDeclaration;
+import net.sourceforge.pmd.ast.ASTImportDeclaration;
+import net.sourceforge.pmd.ast.ASTName;
 import net.sourceforge.pmd.ast.ASTVariableInitializer;
 
 public final class DontPassOtherClassToLoggerFactory extends AbstractJavaRule {
 	// stack which contains class name
 	private Deque<String> stack = new LinkedList<String>();
 
+	private Map<String, String> classNameToFqcn = new HashMap<String, String>();
+
+	private String packageName;
+
+	@Override
+	public Object visit(ASTImportDeclaration node, Object data) {
+		String fqcn = node.getFirstChildOfType(ASTName.class).getImage();
+		String className = fqcn.substring(fqcn.lastIndexOf('.'));
+		classNameToFqcn.put(className, fqcn);
+		return super.visit(node, data);
+	}
+
 	@Override
 	public Object visit(ASTClassOrInterfaceDeclaration node, Object data) {
-		stack.push(node.getImage());
+		packageName = node.getScope().getEnclosingSourceFileScope().getPackageName();
+		String fqcn = packageName + "." + node.getImage();
+		stack.push(fqcn);
 
 		try {
 			return super.visit(node, data);
@@ -30,9 +48,18 @@ public final class DontPassOtherClassToLoggerFactory extends AbstractJavaRule {
 		if (field != null && field.getType().equals(org.slf4j.Logger.class)) {
 			ASTVariableInitializer initializer = node.getFirstChildOfType(ASTVariableInitializer.class);
 			ASTClassOrInterfaceType givenClassToFactory = initializer.getFirstChildOfType(ASTClassOrInterfaceType.class);
-			if (givenClassToFactory!=null) givenClassToFactory.dump(stack.peek() + ">");
-			if (givenClassToFactory != null && !givenClassToFactory.getImage().equals(stack.peek())) {
-				addViolation(data, node);
+			if (givenClassToFactory != null) {
+				String givenClassName = givenClassToFactory.getImage();
+				String fqcn;
+				if (!givenClassName.contains(".")) {
+					fqcn = packageName + "." + givenClassName;
+				} else {
+					fqcn = givenClassName;
+				}
+
+				if (!fqcn.equals(stack.peek())) {
+					addViolation(data, node);
+				}
 			}
 		}
 
