5640f7cb1bb1016bd0525c1ee0b4d53254aed781
eller86 <skypencil+github@gmail.com>
2012-03-18 19:49:55 +0800

    fixing bug in example

--- ruleset/src/test/java/jp/skypencil/pmd/slf4j/example/GivingFqcnToFactory.java
+++ ruleset/src/test/java/jp/skypencil/pmd/slf4j/example/GivingFqcnToFactory.java
@@ -5,5 +5,5 @@ import org.slf4j.LoggerFactory;
 
 public class GivingFqcnToFactory {
 	@SuppressWarnings("unused")
-	private final static Logger LOGGER = LoggerFactory.getLogger(jp.skypencil.pmd.slf4j.example.GivingFqcnToFactory.class);
+	private final Logger LOGGER = LoggerFactory.getLogger(jp.skypencil.pmd.slf4j.example.GivingFqcnToFactory.class);
 }
